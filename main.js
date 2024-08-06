044bb500d993c53cbdaeac9f5fd6b1acb8e6976b
diego.oberto <diegooberto9@gmail.com>
2024-08-06 14:27:06 -0400

    events added

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const container = document.getElementById("container");
 
-
+function coloresRandom(){
+const colores = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+color="#";
+coloresaSize=colores.length
+for (let i =0 ; i < 6; i++){
+randomletter= Math.floor(Math.random()* coloresaSize)
+color += colores[randomletter];
+}
+return color
+}
 
 
 function CrearHilera(padre){
@@ -11,6 +20,15 @@ const cuadrado = document.createElement("div");
 cuadrado.classList.add("style-1");
 
 cuadrado.textContent=`este cuadro es ${i + 1}`;
+cuadrado.addEventListener("mouseover",() => {
+
+    cuadrado.style.backgroundColor = coloresRandom();
+    console.log(cuadrado.style.backgroundColor);
+    
+} )
+// cuadrado.addEventListener("click", () =>{
+//     cuadrado.style.backgroundColor= coloresRandom()
+// })
 
 padre.appendChild(cuadrado);
 }};
@@ -24,4 +42,9 @@ rectangulo.classList.add("style-2");
 CrearHilera(rectangulo);
 
 container.appendChild(rectangulo);
+
+
 };
+
+
+
